ef1bc404a4f2d773cfaaf5fccb9ecd6ef0338651
Opionz <ope.ajayi@hotmail.com>
2021-11-01 05:18:54 +0100

    Add project files.

---
+++ WordCount/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace WordCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kindly input your sentence below :");
+            string sentence = Console.ReadLine();
+            int count = WordCountProcessor(sentence);
+            Console.WriteLine($"The sentence you entered has {count} words in it");
+        }
+
+        static int WordCountProcessor(string input)
+        {
+            int result = 0;
+
+            // Remove all whitespace
+            string newSenctence = input.Trim();
+
+            // Check if entry is null and return a defaut value
+            if (newSenctence == "")
+                return 0;
+
+            
+
+            foreach (string word in newSenctence.Split(' '))
+            {
+                result++;
+            }
+            return result;
+        }
+    }
+}
